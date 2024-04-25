202ef2e3ca84be2fb339a3b2d5fa6e346e0f6997
imp111 <alexsandartenev@gmail.com>
2024-04-25 18:19:38 +0300

    userdetails implementation

---
+++ src/main/java/blog/config/BlogUserDetails.java
@@ -0,0 +1,63 @@
+package blog.config;
+
+import blog.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class BlogUserDetails extends User implements UserDetails {
+    private ArrayList<String> roles;
+    private User user; // our entity type User
+
+    public BlogUserDetails(User user, ArrayList<String> roles) {
+        super(user.getEmail(), user.getFullName(), user.getPassword());
+
+        this.roles = roles;
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String userRoles = StringUtils.collectionToCommaDelimitedString(this.roles);
+
+        return AuthorityUtils.commaSeparatedStringToAuthorityList(userRoles);
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
